f0502931415a31b26b8d7a9d1b924e4ed2d125bb
Sebastian <Marines>
2020-01-31 12:00:22 -0600

    Add slug field

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Servicio)
-admin.site.register(models.SubServicio)
+
+class ServiceAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(models.Servicio, ServiceAdmin)
+admin.site.register(models.SubServicio, ServiceAdmin)
--- web/models.py
+++ web/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Servicio(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
@@ -12,6 +13,7 @@ class Servicio(models.Model):
 class SubServicio(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    slug = models.SlugField()
     parent = models.ForeignKey(Servicio, on_delete=models.CASCADE)
 
     def __str__(self):
