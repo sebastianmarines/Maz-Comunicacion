9da0312e00b67c50045276a889d4919f7ff9061a
Sebastian <Marines>
2020-01-31 22:02:36 -0600

    Create post and blog views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from . import views
+
 app_name = "blog"
 
 urlpatterns = [
-
+    path('', views.PostList.as_view(), name="blog"),
+    path('<slug:slug>', views.PostDetail.as_view(), name="post")
 ]
