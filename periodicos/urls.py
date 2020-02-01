19b66a347f3d9bedd729bec48b61db764024120b
Sebastian <Marines>
2020-01-31 21:19:56 -0600

    Redirect to single news when clicking image

--- periodicos/urls.py
+++ periodicos/urls.py
@@ -5,4 +5,10 @@ from . import views
 app_name = "periodicos"
 
 urlpatterns = [
+    path('portadas-y-columnas/', include([
+        path('', views.portadas_list, name="portadas"),
+        path('mexico/', views.portadas_list_mexico, name="portadas_mexico"),
+        path('mexico/<slug:slug>/', views.portada_mexico, name="portada_mexico"),
+        path('<slug:slug>/', views.portada, name="portada"),
+    ]))
 ]
--- periodicos/views.py
+++ periodicos/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+from .models import PortadasYColumnas
+
+def portadas_list(request):
+    objs = PortadasYColumnas.objects.filter(category=1)
+    context = {
+        'title': "Portadas y Columnas",
+        'mexico': False,
+        'periodicos': objs
+    }
+
+    return render(request, 'portadas-y-columnas.html', context)
+
+
+def portada(request, slug):
+    return HttpResponse(slug)
+
+
+def portadas_list_mexico(request):
+    objs = PortadasYColumnas.objects.filter(category=2)
+    context = {
+        'title': "Portadas y Columnas de México",
+        'mexico': True,
+        'periodicos': objs
+    }
+
+    return render(request, 'portadas-y-columnas.html', context)
+
+
+def portada_mexico(request, slug):
+    return HttpResponse(slug)
