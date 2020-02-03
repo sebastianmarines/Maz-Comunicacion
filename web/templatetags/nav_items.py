f2bd47acdc3094fd8fd9aee32ce2e326d6d338f0
Sebastian Marines <sebastian0marines@gmail.com>
2020-02-03 14:04:07 -0600

    Add services link in footer

--- web/templatetags/nav_items.py
+++ web/templatetags/nav_items.py
@@ -1,4 +1,5 @@
 from django import template
+from django.urls import reverse
 from web.models import Servicio
 
 register = template.Library()
@@ -7,4 +8,9 @@ register = template.Library()
 def get_services_list():
     services = Servicio.objects.all()
     return services
+
+@register.simple_tag
+def service_url():
+    path = reverse('web:index') + '#servicios'
+    return path
 
