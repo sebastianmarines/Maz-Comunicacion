4a2c002f39908e425f1180ef0da43e4c329272b4
Sebastian Marines <sebastian0marines@gmail.com>
2020-02-03 15:15:10 -0600

    Add filter options in sub service admin

--- web/admin.py
+++ web/admin.py
@@ -6,5 +6,10 @@ class ServiceAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+class SubServiceAdmin(ServiceAdmin):
+    list_display = ("title", "parent")
+    list_filter = ("parent",)
+
+
 admin.site.register(models.Servicio, ServiceAdmin)
-admin.site.register(models.SubServicio, ServiceAdmin)
+admin.site.register(models.SubServicio, SubServiceAdmin)
